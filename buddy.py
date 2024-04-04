bdf7d4a1a868c18f42d3231853037f54b46c93e1
Samuel12209 <samuelbenitez12290@gmail.com>
2024-04-04 14:27:16 -0400

    Buddy updated

--- buddy.py
+++ buddy.py
@@ -3,33 +3,38 @@ import time
 import random
 pygame.init()
 
+food_amount_max = 100
 random_number = random.randint(1, 5)
 window_width = 100
 window_height = 70
 screen = pygame.display.set_mode((window_width, window_height))
-BLACK = (0, 0, 0)
-screen.fill(BLACK)
 loop = 0
+backround_image = pygame.image.load("pixelworld.png")
+screen.blit(backround_image, (-5, -30))
 pygame.display.set_caption("Buddy")
 clock = pygame.time.Clock()
 slimex, slimey = 27, 35
 Slime = pygame.image.load("slime.png").convert_alpha()
 pygame.display.set_icon(Slime)
-screen.fill(BLACK)
 action = False
 print("slime smile")
 Slime = pygame.image.load("slime.png").convert_alpha()
 Slime = pygame.transform.scale(Slime, (50, 40))
-screen.fill(BLACK)
 screen.blit(Slime, (slimex, slimey))
+foodicon = pygame.image.load("minecraft_hunger icon.png").convert_alpha()
+foodicon = pygame.transform.scale(foodicon, (25, 20))
+screen.blit(foodicon, (3, 3))
 pygame.display.update()
 
 PLACEHOLDER = "slime.png"
 heart = "heart.png"
 slime_smile = "slime_smile.png"
+slime_smile2 = "slime_smile2.png"
 slime_eating = "slime_eat1.png"
 slime_neutral = "slime.png"
 slime_mouth_closed = "slime_eat2.png"
+food_icon = "minecraft_hunger icon.png"
+
 
 running = True
 while running:
@@ -45,15 +50,22 @@ while running:
             print("slime smile")
             Slime = pygame.image.load(slime_smile).convert_alpha()
             Slime = pygame.transform.scale(Slime, (50, 40))
-            screen.fill(BLACK)
+            screen.blit(backround_image, (-5, -30))
             screen.blit(Slime, (slimex, slimey))
             pygame.display.update()
+            time.sleep(0.25)
+            print("Smile 2")
+            Slime = pygame.image.load(slime_smile2).convert_alpha()
+            Slime = pygame.transform.scale(Slime, (50, 40))
+            screen.blit(backround_image, (-5, -30))
+            screen.blit(Slime, (slimex, slimey))
+            pygame.display.update()
+            time.sleep(0.5)
             print("slime normal")
             Slime = pygame.image.load(slime_neutral).convert_alpha()
             Slime = pygame.transform.scale(Slime, (50, 40))
-            screen.fill(BLACK)
+            screen.blit(backround_image, (-5, -30))
             screen.blit(Slime, (slimex, slimey))
-            pygame.time.delay(1000)
             pygame.display.update()
             action = False
 
@@ -61,8 +73,13 @@ while running:
         action = True
         if 27 < mouse_pos[0] < 77 and 35 < mouse_pos[1] < 75:
             print("Feeding")
-            Slime = pygame.image.load(slime_eating).convert_alpha()
+            Slime = pygame.image.load(slime_smile2).convert_alpha()
             Slime = pygame.transform.scale(Slime, (50, 40))
+            screen.blit(Slime, (slimex, slimey))
+            pygame.display.update()
+            time.sleep(1)
+            Slime = pygame.image.load(slime_eating).convert_alpha()
+            slime = pygame.transform.scale(Slime, (50, 40))
             screen.blit(Slime, (slimex, slimey))
             pygame.display.update()
             time.sleep(1)
@@ -75,6 +92,7 @@ while running:
             Slime = pygame.transform.scale(Slime, (50, 40))
             screen.blit(Slime, (slimex, slimey))
             pygame.display.update()
+
 
 
 pygame.display.update()
